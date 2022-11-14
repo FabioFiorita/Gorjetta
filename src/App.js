e6caa96b2be9890ba72649673a9c5dc026e33420
Fabio Fiorita <fabiolfp@gmail.com>
2022-11-14 19:55:06 -0300

    Adicionando os componentes de formulário e display ao aplicativo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+// style
+import "./App.css";
+
+// components
+import Display from "./components/Display";
+import Form from "./components/Form";
+
 
 function App() {
   return (
-    <div className="App">
-          Learn React
+    <div className="wrapper">
+      <h1 className="title">Gorjetta</h1>
+      <div className="container">
+        <Form />
+        <Display />
+      </div>
     </div>
   );
 }
