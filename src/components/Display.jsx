2020f497eddac65ba4dfd273ed830e0c721d07ad
Fabio Fiorita <fabiolfp@gmail.com>
2022-11-15 15:23:39 -0300

    Atualizando o componente display com as informações do form

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,23 +1,29 @@
-const Display = () => {
+const Display = ({ calculatedTip, calculatedTotal, calculatedPerPerson, toggleResetButton, handleReset }) => {
   return (
     <div className="display">
       <div className="display-group">
           <div className="display-row">
             <div className="display-label">
-              <p className="header">Valor da Gorjeta</p>
-              <p className="unit"> pessoa</p>
+              <p className="header">Valor da Gorjjeta</p>
             </div>
-            <p className="display-amt">R$ 0,00</p>
+            <p className="display-amt">{"R$ " + calculatedTip.toFixed(2).toString().replace(".",",")}</p>
           </div>
           <div className="display-row">
             <div className="display-label">
-              <p className="header">Valor da Gorjeta</p>
+              <p className="header">Valor da Conta</p>
+              <p className="unit"> total</p>
+            </div>
+            <p className="display-amt">{"R$ " + calculatedTotal.toFixed(2).toString().replace(".",",")}</p>
+          </div>
+          <div className="display-row">
+            <div className="display-label">
+              <p className="header">Valor da Conta</p>
               <p className="unit"> pessoa</p>
             </div>
-            <p className="display-amt">R$ 0,00</p>
+            <p className="display-amt">{"R$ " + calculatedPerPerson.toFixed(2).toString().replace(".",",")}</p>
           </div>
       </div>
-      <button className="btn" disabled>Resetar</button>
+      <button className="reset-btn" disabled={!toggleResetButton} onClick={handleReset}>Resetar</button>
     </div>
   );
 };
